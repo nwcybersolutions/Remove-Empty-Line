c9770d805aabcbf38e8d54f8eba0950fa87b74fe
Northwest Cyber Solutions <31363854+nwcybersolutions@users.noreply.github.com>
2017-09-01 18:03:47 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,9 +12,10 @@ License URI: https://opensource.org/licenses/MIT
 Text Domain: Remove Empty Lines
 Domain Path: /languages
 */
-function remove_empty_lines( $content ){
-  // replace empty lines
-  $content = preg_replace("/&nbsp;/", "", $content);
-  return $content;
+function remove_empty_p( $content ) {
+    $content = force_balance_tags( $content );
+    $content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
+    $content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
+    return $content;
 }
-add_action('content_save_pre', 'remove_empty_lines');
+add_filter('the_content', 'remove_empty_p', 20, 1);
