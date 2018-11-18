247fd15b11309881aaded9da9b54d26254ff1328
Northwest Cyber Solutions <31363854+nwcybersolutions@users.noreply.github.com>
2018-11-18 15:41:47 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 Plugin Name: Remove Empty Lines
@@ -6,16 +5,16 @@ GitHub Plugin URI: https://github.com/nwcybersolutions/remove-empty-line/
 Description: Removes "/& n b s p ;/" when using the Visual Editor
 Author: Northwest Cyber Solutions
 Author URI: https://nwcybersolutions.com
-Version: 1.0.2
+Version: 1.0.1
 License: MIT
 License URI: https://opensource.org/licenses/MIT
 Text Domain: Remove Empty Lines
 Domain Path: /languages
 */
-function remove_empty_lines_p( $content ) {
+function remove_empty_p( $content ) {
     $content = force_balance_tags( $content );
     $content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
     $content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
     return $content;
 }
-add_filter('the_content', 'remove_empty_lines_p', 20, 1);
+add_filter('the_content', 'remove_empty_p', 20, 1);
