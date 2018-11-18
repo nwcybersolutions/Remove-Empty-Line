d359132db8fe198c9e9c9b73891739673c9ec401
Northwest Cyber Solutions <31363854+nwcybersolutions@users.noreply.github.com>
2018-11-18 15:09:04 -0800

    Update functions.php
    Fixed WP Error _headers already defined

--- functions.php
+++ functions.php
@@ -12,10 +12,10 @@ License URI: https://opensource.org/licenses/MIT
 Text Domain: Remove Empty Lines
 Domain Path: /languages
 */
-function remove_empty_p( $content ) {
+function remove_empty_lines_p( $content ) {
     $content = force_balance_tags( $content );
     $content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
     $content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
     return $content;
 }
-add_filter('the_content', 'remove_empty_p', 20, 1);
+add_filter('the_content', 'remove_empty_lines_p', 20, 1);
